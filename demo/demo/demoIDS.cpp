7df637f4e28cf73e67f41639d1aabb72b5716670
Thomas Obst <obst@hlrs.de>
2021-09-08 09:48:25 +0200

    ids camera demo

---
+++ demo/demo/demoIDS.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <signal.h>
+#include <stdlib.h> /* srand, rand */
+#include <unistd.h>
+#include <mutex>
+#include <https_stream.h> //https_stream
+
+#include <cstdint>
+#include <peak/converters/peak_buffer_converter_ipl.hpp>
+#include <peak/peak.hpp>
+#include <peak_ipl/peak_ipl.hpp>
+
+#include "CenternetDetection.h"
+#include "MobilenetDetection.h"
+#include "Yolo3Detection.h"
+
+bool gRun;
+bool SAVE_RESULT = false;
+
+void sig_handler(int signo)
+{
+    std::cout << "request gateway stop\n";
+    gRun = false;
+}
+
+int main(int argc, char *argv[])
+{
+
+    std::cout << "detection\n";
+    signal(SIGINT, sig_handler);
+    
+    // JSON-Port
+    int json_port = find_int_arg(argc, argv, "-json_port", -1);
+  
+    // Net
+    char *inputnet = find_char_arg(argc, argv, "-net", "yolo3_berkeley.rt");
+    std::string net(inputnet);
+
+    // Input 
+    char *inputvideo = find_char_arg(argc, argv, "-input", "../demo/yolo_test.mp4");
+    std::string input(inputvideo); 
+
+    //Net-Type  
+    char *input_ntype = find_char_arg(argc, argv, "-ntype", "y");
+    char ntype = input_ntype[0];
+
+    int n_classes = find_int_arg(argc, argv, "-n_classes", 80);
+    
+    int n_batch = find_int_arg(argc, argv, "-n_batch", 1);
+
+    int show = find_int_arg(argc, argv, "-show", 1);
+
+    int save = find_int_arg(argc, argv, "-save", 0);
+    SAVE_RESULT = save;
+
+    int mjpeg_port = find_int_arg(argc, argv, "-mjpeg_port", 0);
+  
+    int extyolo = find_int_arg(argc, argv, "-extyolo", 0);
+
+    int video_mode = find_int_arg(argc, argv, "-video_mode", 0);
+
+    if (n_batch < 1 || n_batch > 64)
+        FatalError("Batch dim not supported");
+
+
+    tk::dnn::Yolo3Detection yolo;
+    tk::dnn::CenternetDetection cnet;
+    tk::dnn::MobilenetDetection mbnet;
+
+    tk::dnn::DetectionNN *detNN;
+
+    switch (ntype)
+    {
+    case 'y':
+        detNN = &yolo;
+        break;
+    case 'c':
+        detNN = &cnet;
+        break;
+    case 'm':
+        detNN = &mbnet;
+        n_classes++;
+        break;
+    default:
+        FatalError("Network type not allowed (3rd parameter)\n");
+    }
+
+    detNN->init(net, n_classes, n_batch);
+
+    gRun = true;
+
+    cv::VideoCapture cap(input);
+    if (!cap.isOpened())
+    {
+        gRun = false;
+    }
+    else
+    {
+        std::cout << "camera started\n";
+        cap.set(cv::CAP_PROP_FOURCC,cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+        switch (video_mode)
+        {
+            case 0:
+                cap.set(cv::CAP_PROP_FRAME_WIDTH,1920);
+                cap.set(cv::CAP_PROP_FRAME_HEIGHT,1080);
+                break;
+            case 1:
+                cap.set(cv::CAP_PROP_FRAME_WIDTH,3840);
+                cap.set(cv::CAP_PROP_FRAME_HEIGHT,2160);
+                break;
+            default:
+                cap.set(cv::CAP_PROP_FRAME_WIDTH,1920);
+                cap.set(cv::CAP_PROP_FRAME_HEIGHT,1080);
+
+        }
+        int w = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+        int h = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+        std::cout << "Width: " << w << " Height: " << h << "\n";
+    }
+
+    cv::VideoWriter resultVideo;
+    if (SAVE_RESULT)
+    {
+        int w = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+        int h = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+        resultVideo.open("result.mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, cv::Size(w, h));
+    }
+
+    cv::Mat frame;
+    if (show)
+        cv::namedWindow("detection", cv::WINDOW_NORMAL);
+    cv::moveWindow("detection", 100, 100);
+    cv::resizeWindow("detection", 1920, 1080);
+
+    std::vector<cv::Mat> batch_frame;
+    std::vector<cv::Mat> batch_dnn_input;
+
+    long long int frame_id = 0;
+
+    while (gRun)
+    {
+        batch_dnn_input.clear();
+        batch_frame.clear();
+
+        for (int bi = 0; bi < n_batch; ++bi)
+        {
+            cap >> frame;
+            if (!frame.data)
+                break;
+
+            batch_frame.push_back(frame);
+
+            // this will be resized to the net format
+            batch_dnn_input.push_back(frame.clone());
+        }
+        if (!frame.data)
+            break;
+
+        //inference
+        detNN->update(batch_dnn_input, n_batch);
+        detNN->draw(batch_frame,extyolo);
+
+        if (show)
+        {
+            for (int bi = 0; bi < n_batch; ++bi)
+            {
+                cv::imshow("detection", batch_frame[bi]);
+            }
+        }
+        if (cv::waitKey(1) == 27)
+        {
+            break;
+        }
+        if (n_batch == 1 && SAVE_RESULT)
+            resultVideo << frame;
+
+        if (mjpeg_port > 0)
+        {
+            send_mjpeg(batch_frame[0], mjpeg_port, 400000, 40);
+        }
+        
+        if (json_port > 0)
+        {
+            send_json(batch_frame, *detNN, frame_id, json_port, 40000);
+            frame_id++;
+        }
+    }
+
+    std::cout << "detection end\n";
+    double mean = 0;
+
+    std::cout << COL_GREENB << "\n\nTime stats:\n";
+    std::cout << "Min: " << *std::min_element(detNN->stats.begin(), detNN->stats.end()) / n_batch << " ms\n";
+    std::cout << "Max: " << *std::max_element(detNN->stats.begin(), detNN->stats.end()) / n_batch << " ms\n";
+    for (int i = 0; i < detNN->stats.size(); i++)
+        mean += detNN->stats[i];
+    mean /= detNN->stats.size();
+    std::cout << "Avg: " << mean / n_batch << " ms\t" << 1000 / (mean / n_batch) << " FPS\n"
+              << COL_END;
+
+    return 0;
+}
