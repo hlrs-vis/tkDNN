d0f975bf6c8b24fe71cf80f5dfad4b72f5a969bc
tobst <obst@hlrs.de>
2021-12-09 11:45:36 +0100

    new location of tkdnn

--- tkdnn-start.sh
+++ tkdnn-start.sh
@@ -8,5 +8,5 @@ v4l2-ctl -d 0 -c led1_mode=0
 v4l2-ctl -d 0 -c exposure_auto_priority=1
 v4l2-ctl -d 0 -c backlight_compensation=0
 
-cd /mnt/sd-card/software/tkDNN/build/
+cd /usr/local/src/tkDNN/build/
 ./demoGeneral -ini camera1.ini 
