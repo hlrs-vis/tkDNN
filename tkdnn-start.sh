b9088e838b6ac95a32979fafbbeae987846299fc
Thomas Obst <obst@hlrs.de>
2022-02-24 15:04:19 +0100

    config in project root

--- tkdnn-start.sh
+++ tkdnn-start.sh
@@ -8,5 +8,5 @@ v4l2-ctl -d 0 -c led1_mode=0
 v4l2-ctl -d 0 -c exposure_auto_priority=1
 v4l2-ctl -d 0 -c backlight_compensation=0
 
-cd /usr/local/src/tkDNN/build/
-./demoGeneral -ini config_visagx1-brio.ini 
+cd /usr/local/src/tkDNN/build
+./demoGeneral -ini ../config_visagx1-brio.ini 
