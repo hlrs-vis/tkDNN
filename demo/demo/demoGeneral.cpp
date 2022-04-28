72f7c4ee6461c1300a78abf0f0ae5f1636db8d64
Thomas Obst <obst@hlrs.de>
2022-04-28 17:01:35 +0200

    draw detections

--- demo/demo/demoGeneral.cpp
+++ demo/demo/demoGeneral.cpp
@@ -586,7 +586,7 @@ if (json)
         //inference
         detNN->update(batch_dnn_input, n_batch);
         if (draw)
-            //detNN->draw(batch_frame,extyolo);
+            detNN->draw(batch_frame,extyolo);
 
         if (show)
         {
