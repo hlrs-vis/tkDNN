67810b522fe5991660f44120cba58b146f8e27b5
visagx <thomas.obst@hlrs.de>
2022-12-05 10:28:34 +0100

    add exposure settings

--- tkdnn-start.sh
+++ tkdnn-start.sh
@@ -16,6 +16,8 @@ v4l2-ctl -d 0 -c focus_absolute=0
 v4l2-ctl -d 0 -c led1_mode=0
 v4l2-ctl -d 0 -c exposure_auto_priority=1
 v4l2-ctl -d 0 -c backlight_compensation=0
+v4l2-ctl -d 0 -c exposure_auto=1
+v4l2-ctl -d 0 -c exposure_absolute=20
 
 cd /usr/local/src/tkDNN/build
 ./demoGeneral -ini ../config_visagx-brio.ini 
