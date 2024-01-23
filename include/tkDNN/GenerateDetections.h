a5be661f663cf548364164d5e28d305aabad15e5
Thomas Obst <obst@hlrs.de>
2024-01-23 11:47:14 +0100

    correct typo

--- include/tkDNN/GenerateDetections.h
+++ include/tkDNN/GenerateDetections.h
@@ -32,6 +32,6 @@ class ImageEncoder{
 // create_box_encoder creates the function to encode detections
 cv::Mat create_box_encoder(const std::string &model_filename, const std::string &input_name, const std::string &output_name , const int batch_size);
 
-cv::Mat generateDetections(std::function<auto(cv::Mat, vector<cv::Rect>)> encoder, std::vector<TypewithMetadata<cv::Mat>> *batch_images, tk::dnn::detectionNN &detNN);
+cv::Mat generateDetections(std::function<auto(cv::Mat, vector<cv::Rect>)> encoder, std::vector<TypewithMetadata<cv::Mat>> *batch_images, tk::dnn::DetectionNN &detNN);
 
 #endif 
